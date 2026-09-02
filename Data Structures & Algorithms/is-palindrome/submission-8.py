abbb7f4a93140e64dc1713660b8d72751749241a
Shrey Kumar <156003188+shreykumar12@users.noreply.github.com>
2026-09-02 17:08:06 -0400

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sClean = ""
+
+        for c in s:
+            if c.isalnum():
+                sClean += c.lower()
+        print(sClean)
+        l, r = 0, len(sClean) - 1
+
+        while l < r:
+            if sClean[l] != sClean[r]:
+                return False
+            l += 1
+            r -= 1
+        
+        return True
